2282426af6679fbc41c9aa5f3fd9455c0ce00a7b
Yorik van Havre <yorik@uncreated.net>
2019-01-20 15:28:11 -0200

    Further work on preflight tool

--- BimPreflight.py
+++ BimPreflight.py
@@ -22,7 +22,7 @@
 
 """This module contains FreeCAD commands for the BIM workbench"""
 
-import os,FreeCAD,FreeCADGui,Draft,Arch,Part,csv
+import os,FreeCAD,FreeCADGui,Draft,Arch,Part,csv,re
 from PySide import QtCore,QtGui
 
 def QT_TRANSLATE_NOOP(ctx,txt): return txt # dummy function for the QT translator
@@ -43,6 +43,7 @@ tests = ["testAll",
          "testExtrusions",
          "testStandardCases",
          "testTinyLines",
+         "testRectangleProfileDef",
         ]
 
 class BIM_Preflight:
@@ -85,6 +86,7 @@ class BIM_Preflight_TaskPanel:
 
     def reject(self):
 
+        QtGui.QApplication.restoreOverrideCursor()
         FreeCADGui.Control.closeDialog()
         FreeCAD.ActiveDocument.recompute()
 
@@ -133,7 +135,8 @@ class BIM_Preflight_TaskPanel:
                 self.rform.buttonReport.clicked.connect(self.toReport)
                 self.rform.buttonOK.clicked.connect(self.closeReport)
             self.rform.textBrowser.setText(self.results[test])
-            self.rform.label.setText("Results of "+test+":")
+            label = test.replace("test","label")
+            self.rform.label.setText(getattr(self.form,label).text())
             self.rform.test = test
             self.rform.show()
 
@@ -176,6 +179,17 @@ class BIM_Preflight_TaskPanel:
         return objs
 
 
+    def getToolTip(self,test):
+
+        "gets the toolTip text from the ui file"
+
+        label = test.replace("test","label")
+        tooltip = getattr(self.form,label).toolTip()
+        tooltip = tooltip.replace("</p>","</p>\n\n")
+        tooltip = re.sub("<.*?>","",tooltip) # strip html tags
+        return tooltip
+
+
     def testAll(self):
 
         "runs all tests"
@@ -212,14 +226,9 @@ class BIM_Preflight_TaskPanel:
                 if ifcopenshell.schema_identifier.startswith("IFC4"):
                     self.passed(test)
                 else:
-                    msg = "The version of ifcopenshell installed on your system will produce files with this schema version:\n\n"
+                    msg = self.getToolTip(test)
+                    msg += "The version of ifcopenshell installed on your system will produce files with this schema version:\n\n"
                     msg += ifcopenshell.schema_identifier + "\n\n"
-                    msg += "IFC export in FreeCAD is performed by an open-source third-party library called IfcOpenShell. "
-                    msg += "To be able to export to the newer IFC4 standard, IfcOpenShell must have been compiled with IFC4 "
-                    msg += "support enabled. This test checks if IFC4 support is available in your version of IfcOpenShell. "
-                    msg += "If not, you will only be able to export IFC files in the older IFC2x3 standard. Note that some "
-                    msg += "applications out there still have incomplete or inexistant IFC4 support, so in some cases "
-                    msg += "IFC2x3 might still work better."
                     self.failed(test)
             self.results[test] = msg
 
@@ -248,18 +257,14 @@ class BIM_Preflight_TaskPanel:
                 elif (hasattr(obj,"IfcRole") and (obj.IfcRole == "Building Storey")):
                     storeys = True
             if (not sites) or (not buildings)  or (not storeys):
-                msg = "The IFC standard requires at least one site, one building and one level or building storey per project. "
-                msg += "The following types were not found in the project: "
+                msg = self.getToolTip(test)
+                msg += "The following types were not found in the project:\n"
                 if not sites:
-                    msg += "Site, "
+                    msg += "\nSite"
                 if not buildings:
-                    msg += "Building, "
+                    msg += "\nBuilding"
                 if not storeys:
-                    msg += "Building Storeys, "
-                msg += "\n\nNote that, as this is a mandatory requirement, FreeCAD will automatically add a default site, "
-                msg += "a default building and/or a default building storey if any of these is missing. So even if this "
-                msg += "test didn't pass, your exported IFC file will meet the requirements. However, it is always better "
-                msg += "to create these objects yourself, as you get more control over naming and properties."
+                    msg += "\nBuilding Storey"
             if msg:
                 self.failed(test)
             else:
@@ -293,7 +298,8 @@ class BIM_Preflight_TaskPanel:
                     if not ok:
                         self.culprits[test].append(obj)
                         if not msg:
-                            msg = "The following Building objects have been found to not be included in any Site. "
+                            msg = self.getToolTip(test)
+                            msg += "The following Building objects have been found to not be included in any Site. "
                             msg += "You can resolve the situation by creating a Site object, if none is present "
                             msg += "in your model, and drag and drop the Building objects into it in the tree view:\n\n"
                         msg += obj.Label +"\n"
@@ -330,7 +336,8 @@ class BIM_Preflight_TaskPanel:
                     if not ok:
                         self.culprits[test].append(obj)
                         if not msg:
-                            msg = "The following Building Storey (BuildingParts with their IFC role set as \"Building Storey\")"
+                            msg = self.getToolTip(test)
+                            msg += "The following Building Storey (BuildingParts with their IFC role set as \"Building Storey\")"
                             msg += "objects have been found to not be included in any Building. "
                             msg += "You can resolve the situation by creating a Building object, if none is present "
                             msg += "in your model, and drag and drop the Building Storey objects into it in the tree view:\n\n"
@@ -367,10 +374,11 @@ class BIM_Preflight_TaskPanel:
                     if not ok:
                         self.culprits[test].append(obj)
                         if not msg:
-                            msg = "The following BIM objects have been found to not be included in any Building Storey "
+                            msg = self.getToolTip(test)
+                            msg += "The following BIM objects have been found to not be included in any Building Storey "
                             msg += "(BuildingParts with their IFC role set as \"Building Storey\"). "
                             msg += "You can resolve the situation by creating a Building Storey object, if none is present "
-                            msg += "in your model, and drag and drop the BIM objects into it in the tree view:\n\n"
+                            msg += "in your model, and drag and drop these objects into it in the tree view:\n\n"
                         msg += obj.Label +"\n"
             if msg:
                 self.failed(test)
@@ -405,11 +413,7 @@ class BIM_Preflight_TaskPanel:
                     self.culprits[test].append(obj)
                     notbim.append(obj)
             if undefined or notbim:
-                msg = "Some of the objects have no IFC type defined. They will therefore be exported as the generic "
-                msg += "IfcBuildingElementProxy type. This is not a problem, and might even be what you want. In some "
-                msg += "cases, this type is even recommended as it might prevent host applications to perform unwanted "
-                msg += "transformations on the object. However, since IFC is all about semantics, it is often interesting "
-                msg += "to give a defined type to all your objects.\n\n"
+                msg = self.getToolTip(test)
                 if undefined:
                     msg += "The following BIM objects have the \"Undefined\" type:\n\n"
                     for o in undefined:
@@ -446,10 +450,7 @@ class BIM_Preflight_TaskPanel:
                     if (not obj.Shape.isNull()) and ((not obj.Shape.isValid()) or (not obj.Shape.Solids)):
                         self.culprits[test].append(obj)
             if self.culprits[test]:
-                msg = "Some of the objects have an invalid or non-solid shape. "
-                msg += "Although IFC doesn't require objects to be solid, non-solid objects "
-                msg += "are usually caused by problematic modelling, and will often cause problems in other "
-                msg += "BIM applications.\n\n"
+                msg = self.getToolTip(test)
                 msg += "The following BIM objects have an invalid or non-solid geometry:\n\n"
                 for o in self.culprits[test]:
                     msg += o.Label + "\n"
@@ -483,13 +484,14 @@ class BIM_Preflight_TaskPanel:
                                 self.culprits[test].append(obj)
                                 break
             if self.culprits[test]:
-                msg = "Some of the objects have Length, Width or Height properties, "
+                msg = self.getToolTip(test)
+                msg += "The objects below have Length, Width or Height properties, "
                 msg += "but these properties won't be explicitely exported to IFC. "
                 msg += "This is not necessarily an issue, unless you specifically want these "
                 msg += "quantities to be exported:\n\n"
                 for o in self.culprits[test]:
                     msg += o.Label + "\n"
-                msg += "To enable exporting of these quantities, use the IFC quantities manager tool "
+                msg += "\nTo enable exporting of these quantities, use the IFC quantities manager tool "
                 msg += "located under menu Manage -> Manage IFC Quantities..."
             if msg:
                 self.failed(test)
@@ -523,21 +525,151 @@ class BIM_Preflight_TaskPanel:
             psets = [''.join(map(lambda x: x if x.islower() else " "+x, p)) for p in psets]
 
             for obj in self.getObjects():
+                ok = True
                 if hasattr(obj,"IfcRole") and hasattr(obj,"IfcProperties") and isinstance(obj.IfcProperties,dict):
-                    for role in psets:
-                        if obj.IfcRole == role:
-                            if not "Pset_"+role.replace(" ","")+"Common" in ','.join(obj.IfcProperties.values()):
-                                self.culprits[test].append(obj)
+                    if obj.IfcRole != "Undefined":
+                        ok = False
+                        for role in psets:
+                            if obj.IfcRole == role:
+                                if "Pset_"+role.replace(" ","")+"Common" in ','.join(obj.IfcProperties.values()):
+                                    ok = True
+                                    break
+                if not ok:
+                    self.culprits[test].append(obj)
 
             if self.culprits[test]:
-                msg = "Some of the objects will be exported as a defined IFC type, for example a wall, "
-                msg += "but they don't have the associated default Common Property Set defined. "
-                msg += "This is not necessarily an issue, unless you specifically want your "
-                msg += "objects to have common property sets:\n\n"
+                msg = self.getToolTip(test)
+                msg += "The objects below have a defined IFC type but do not have the associated common property set:\n\n"
                 for o in self.culprits[test]:
                     msg += o.Label + "\n"
                 msg += "To add common property sets to these objects, use the IFC properties manager tool "
                 msg += "located under menu Manage -> Manage IFC Properties..."
+            if msg:
+                self.failed(test)
+            else:
+                self.passed(test)
+            self.results[test] = msg
+            QtGui.QApplication.restoreOverrideCursor()
+
+
+    def testPsets(self):
+
+        "tests for property sets integrity"
+
+        test = "testPsets"
+        if getattr(self.form,test).text() == "Failed":
+            self.show(test)
+        else:
+            QtGui.QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+            self.reset(test)
+            self.results[test] = None
+            self.culprits[test] = []
+            msg = None
+            psets = {}
+            psetspath = os.path.join(FreeCAD.getResourceDir(),"Mod","Arch","Presets","pset_definitions.csv")
+            if os.path.exists(psetspath):
+                with open(psetspath, "r") as csvfile:
+                    reader = csv.reader(csvfile, delimiter=';')
+                    for row in reader:
+                        if "Common" in row[0]:
+                            psets[row[0]] = row[1:]
+ 
+            for obj in self.getObjects():
+                ok = True
+                if hasattr(obj,"IfcRole") and hasattr(obj,"IfcProperties") and isinstance(obj.IfcProperties,dict):
+                    if obj.IfcRole != "Undefined":
+                        found = None
+                        for pset in psets.keys():
+                            for val in obj.IfcProperties.values():
+                                if pset in val:
+                                    found = pset
+                                    break
+                        if found:
+                            for i in range(int(len(psets[found])/2)):
+                                p = psets[found][i*2]
+                                t = psets[found][i*2+1]
+                                #print("testing for ",p,t,found," in ",obj.IfcProperties)
+                                if p in obj.IfcProperties:
+                                    if (not found in obj.IfcProperties[p]) or (not t in obj.IfcProperties[p]):
+                                        ok = False
+                                else:
+                                    ok = False
+                if not ok:
+                    self.culprits[test].append(obj)
+
+            if self.culprits[test]:
+                msg = self.getToolTip(test)
+                msg += "The objects below have a common property set but that property set doesn't contain all the needed properties:\n\n"
+                for o in self.culprits[test]:
+                    msg += o.Label + "\n"
+                msg += "\nVerify which properties a certain property set must contain on http://www.buildingsmart-tech.org/ifc/IFC4/Add2/html/annex/annex-b/alphabeticalorder_psets.htm\n\n"
+                msg += "To fix the property sets of these objects, use the IFC properties manager tool "
+                msg += "located under menu Manage -> Manage IFC Properties..."
+            if msg:
+                self.failed(test)
+            else:
+                self.passed(test)
+            self.results[test] = msg
+            QtGui.QApplication.restoreOverrideCursor()
+
+
+    def testMaterials(self):
+
+        "tests for materials in BIM objects"
+
+        test = "testMaterials"
+        if getattr(self.form,test).text() == "Failed":
+            self.show(test)
+        else:
+            QtGui.QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+            self.reset(test)
+            self.results[test] = None
+            self.culprits[test] = []
+            msg = None
+            for obj in self.getObjects():
+                if "Material" in obj.PropertiesList:
+                    if not obj.Material:
+                        self.culprits[test].append(obj)
+            if self.culprits[test]:
+                msg = self.getToolTip(test)
+                msg += "The following BIM objects have no material attributed:\n\n"
+                for o in self.culprits[test]:
+                    msg += o.Label + "\n"
+            if msg:
+                self.failed(test)
+            else:
+                self.passed(test)
+            self.results[test] = msg
+            QtGui.QApplication.restoreOverrideCursor()
+
+
+    def testStandards(self):
+
+        "tests for standards in BIM objects"
+
+        test = "testStandards"
+        if getattr(self.form,test).text() == "Failed":
+            self.show(test)
+        else:
+            QtGui.QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+            self.reset(test)
+            self.results[test] = None
+            self.culprits[test] = []
+            msg = None
+            for obj in self.getObjects():
+                if "StandardCode" in obj.PropertiesList:
+                    if not obj.StandardCode:
+                        self.culprits[test].append(obj)
+                if "Material" in obj.PropertiesList:
+                    if obj.Material:
+                        if "StandardCode" in obj.Material.PropertiesList:
+                            if not obj.Material.StandardCode:
+                                self.culprits[test].append(obj.Material)
+            if self.culprits[test]:
+                msg = self.getToolTip(test)
+                msg += "The following BIM objects have no defined standard code:\n\n"
+                for o in self.culprits[test]:
+                    msg += o.Label + "\n"
             if msg:
                 self.failed(test)
             else:
@@ -575,7 +707,8 @@ class BIM_Preflight_TaskPanel:
                 result.Shape = Part.makeCompound(edges)
                 result.ViewObject.LineWidth = 5
                 self.culprits[test] = [result]
-                msg = "Some of the objects have lines smaller than 1/32 inch or 0.8mm, which is the smallest "
+                msg = self.getToolTip(test)
+                msg += "The objects below have lines smaller than 1/32 inch or 0.8mm, which is the smallest "
                 msg += "line size that Revit accepts. These objects will be discarded when imported into Revit:\n\n"
                 for obj in objs:
                     msg += obj.Label +"\n"
@@ -589,6 +722,27 @@ class BIM_Preflight_TaskPanel:
                 self.passed(test)
             self.results[test] = msg
             QtGui.QApplication.restoreOverrideCursor()
+
+
+    def testRectangleProfileDef(self):
+
+        "tests for RectangleProfileDef disable"
+
+        test = "testRectangleProfileDef"
+        if getattr(self.form,test).text() == "Failed":
+            self.show(test)
+        else:
+            self.reset(test)
+            self.results[test] = None
+            self.culprits[test] = None
+            msg = None
+            if FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Arch").GetBool("DisableIfcRectangleProfileDef",False):
+                self.passed(test)
+            else:
+                msg = self.getToolTip(test)
+                self.failed(test)
+            self.results[test] = msg
+
 
 
 FreeCADGui.addCommand('BIM_Preflight',BIM_Preflight())
