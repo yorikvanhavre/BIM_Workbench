yorikvanhavre/BIM_Workbench

commit 8bb8bb5aa3eda5cd5f9fee65a2f38868b6a5be1d

Finished late loading optimization

BimIfcExplorer.py
@@ -21,28 +21,10 @@
 #***************************************************************************
 
 from __future__ import print_function
-import FreeCAD
-import FreeCADGui
+
 import os
-import six
-import Mesh
-from DraftTools import translate
-from PySide import QtCore,QtGui
-
-def QT_TRANSLATE_NOOP(ctxt,txt):
-    return txt
-
-# setting up a font
-bold = QtGui.QFont()
-bold.setWeight(75)
-bold.setBold(True)
-
-# setting up a link fint
-linkfont = QtGui.QFont()
-linkfont.setUnderline(True)
-
-# setup a brush to paint text in system link color
-linkbrush = QtGui.QApplication.palette().link()
+import FreeCAD
+from BimTranslateUtils import *
 
 
 class BIM_IfcExplorer:
@@ -56,7 +38,7 @@ class BIM_IfcExplorer:
     def GetResources(self):
 
         import Arch_rc
-        return {'Pixmap'  : ":/icons/IFC.svg",
+        return {'Pixmap'  : os.path.join(os.path.dirname(__file__),"icons","IFC.svg"),
                 'MenuText': QT_TRANSLATE_NOOP("BIM_IfcExplorer", "IFC explorer"),
                 'ToolTip' : QT_TRANSLATE_NOOP("BIM_IfcExplorer", "IFC explorer utility"),
                }
@@ -64,11 +46,25 @@ class BIM_IfcExplorer:
 
     def Activated(self):
 
+        import FreeCADGui
+        from PySide import QtCore,QtGui
         try:
             import ifcopenshell
         except ImportError:
             FreeCAD.Console.PrintError(translate("BIM","IfcOpenShell was not found on this system. IFC support is disabled")+"\n")
             return
+
+        # setting up a font
+        self.bold = QtGui.QFont()
+        self.bold.setWeight(75)
+        self.bold.setBold(True)
+        
+        # setting up a link fint
+        self.linkfont = QtGui.QFont()
+        self.linkfont.setUnderline(True)
+        
+        # setup a brush to paint text in system link color
+        self.linkbrush = QtGui.QApplication.palette().link()
 
         # draw the main tree widget
         self.tree = QtGui.QTreeWidget()
@@ -166,7 +162,7 @@ class BIM_IfcExplorer:
         "opens a file"
         
         import ifcopenshell
-
+        from PySide import QtCore,QtGui
         self.filename = None
         lastfolder = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/BIM").GetString("lastIfcExplorerFolder","")
         filename = QtGui.QFileDialog.getOpenFileName(None,translate("BIM","Select an IFC file"),lastfolder,translate("BIM","IFC files (*.ifc)"))
@@ -225,13 +221,13 @@ class BIM_IfcExplorer:
         
         "inserts selected objects in the active document"
         
+        import importIFC
         doc = FreeCAD.ActiveDocument
         if doc and self.filename:
             item = self.tree.currentItem()
             if item:
                 eid = item.data(0,QtCore.Qt.UserRole)
                 if eid:
-                    import importIFC
                     importIFC.ZOOMOUT = False
                     importIFC.insert(self.filename,doc.Name,only=[eid])
                     if self.currentmesh:
@@ -242,6 +238,9 @@ class BIM_IfcExplorer:
         
         "turns mesh display on/off"
 
+        import FreeCADGui
+        import ifcopenshell
+        from ifcopenshell import geom
         if not FreeCAD.ActiveDocument:
             FreeCAD.newDocument()
         if FreeCAD.ActiveDocument:
@@ -257,8 +256,6 @@ class BIM_IfcExplorer:
                         trf = FreeCAD.Matrix()
                         trf.scale(s,s,s)
                     basemesh = Mesh.Mesh()
-                    import ifcopenshell
-                    from ifcopenshell import geom
                     s = geom.settings()
                     s.set(s.USE_WORLD_COORDS,True)
                     for product in self.products:
@@ -353,6 +350,8 @@ class BIM_IfcExplorer:
 
         "adds a given entity and its children to the given tree item"
 
+        from PySide import QtCore,QtGui
+
         def get_name(e):
             try:
                 return " : " + e.get_info()['Name']
@@ -366,7 +365,7 @@ class BIM_IfcExplorer:
             name = ""
             if entity.is_a("IfcProduct"):
                 name = get_name(entity)
-                item.setFont(0,bold)
+                item.setFont(0,self.bold)
                 if isinstance(parent,QtGui.QTreeWidgetItem):
                     parent.setExpanded(True)
             item.setText(0,"#"+self.tostr(eid)+" : "+self.tostr(entity.is_a())+name)
@@ -412,6 +411,7 @@ class BIM_IfcExplorer:
         "adds the attributes of the given IFC entity under the given QTreeWidgetITem"
 
         import ifcopenshell
+        from PySide import QtCore,QtGui
         
         entity = self.ifc[eid]
 
@@ -445,10 +445,10 @@ class BIM_IfcExplorer:
                         if t and (t != "None"):
                             item.setText(1,t)
                             if colored:
-                                item.setForeground(1,linkbrush)
-                                item.setFont(1,linkfont)
+                                item.setForeground(1,self.linkbrush)
+                                item.setFont(1,self.linkfont)
                             if argname == "Name":
-                                item.setFont(1,bold)
+                                item.setFont(1,self.bold)
                         if isinstance(argvalue,(list,tuple)):
                             j = 0
                             for argitem in argvalue:
@@ -465,14 +465,14 @@ class BIM_IfcExplorer:
                                 if j == 0:
                                     item.setText(1,t)
                                     if colored:
-                                        item.setForeground(1,linkbrush)
-                                        item.setFont(1,linkfont)
+                                        item.setForeground(1,self.linkbrush)
+                                        item.setFont(1,self.linkfont)
                                 else:
                                     subitem = QtGui.QTreeWidgetItem(item)
                                     subitem.setText(1,t)
                                     if colored:
-                                        subitem.setForeground(1,linkbrush)
-                                        subitem.setFont(1,linkfont)
+                                        subitem.setForeground(1,self.linkbrush)
+                                        subitem.setFont(1,self.linkfont)
                                 j += 1
                 i += 1
 
@@ -480,7 +480,8 @@ class BIM_IfcExplorer:
     def addProperties(self,eid,parent):
     
         "adds properties of a given entity to the given QTReeWidgetItem"
-        
+
+        from PySide import QtCore,QtGui
         entity = self.ifc[eid]
         if hasattr(entity,"IsDefinedBy"):
             for rel in entity.IsDefinedBy:
@@ -488,7 +489,7 @@ class BIM_IfcExplorer:
                     if rel.RelatingPropertyDefinition:
                         item = QtGui.QTreeWidgetItem(parent)
                         item.setText(0,"PropertySet: "+self.tostr(rel.RelatingPropertyDefinition.Name))
-                        item.setFont(0,bold)
+                        item.setFont(0,self.bold)
                         self.properties.setFirstItemColumnSpanned(item,True)
                         if hasattr(rel.RelatingPropertyDefinition,"HasProperties"):
                             for prop in rel.RelatingPropertyDefinition.HasProperties:
@@ -501,6 +502,7 @@ class BIM_IfcExplorer:
 
         "resolves py2/py3 string representation hassles"
 
+        import six
         if six.PY2:
             if isinstance(text,unicode):
                 return text.encode("utf8")
@@ -516,7 +518,8 @@ class BIM_IfcExplorer:
     def onSelectTree(self,item,previous):
         
         "displays attributes and properties of a tree item"
-        
+
+        from PySide import QtCore,QtGui
         self.backnav.append(previous)
         eid = item.data(0,QtCore.Qt.UserRole)
         self.attributes.clear()

BimClone.py
@@ -78,3 +78,91 @@ class BIM_ResetCloneColors:
             if hasattr(obj,"CloneOf") and obj.CloneOf:
                 obj.ViewObject.DiffuseColor = obj.CloneOf.ViewObject.DiffuseColor
 
+
+class BIM_Unclone:
+
+
+    def GetResources(self):
+
+        return {'Pixmap'  : os.path.join(os.path.dirname(__file__),"icons","BIM_Unclone.svg"),
+                'MenuText': QT_TRANSLATE_NOOP("BIM_Unclone", "Unclone"),
+                'ToolTip' : QT_TRANSLATE_NOOP("BIM_Unclone", "Makes a selected clone object independent from its original"),
+               }
+
+    def IsActive(self):
+
+        import FreeCADGui
+        if FreeCADGui.Selection.getSelection():
+            return True
+        else:
+            return False
+
+    def Activated(self):
+
+        import FreeCADGui
+        import Draft
+        # get selected object and face
+        sel = FreeCADGui.Selection.getSelection()
+
+        if len(sel) == 1:
+
+            # make this undoable
+            FreeCAD.ActiveDocument.openTransaction("Reextrude")
+            obj = sel[0]
+
+            # check that types are identical
+            if hasattr(obj,"CloneOf") and obj.CloneOf:
+                cloned = obj.CloneOf
+                placement = FreeCAD.Placement(obj.Placement)
+                if Draft.getType(obj) != Draft.getType(cloned):
+                    # wrong type - we need to create a new object
+                    newobj = getattr(Arch,"make"+Draft.getType(cloned))()
+                else:
+                    newobj = obj
+                    newobj.CloneOf = None
+
+                # copy properties over, except special ones
+                for prop in cloned.PropertiesList:
+                    if not prop in ["Objects","CloneOf","ExpressionEngine","HorizontalArea","Area","VerticalArea","PerimeterLength","Proxy","Shape"]:
+                        setattr(newobj,prop,getattr(cloned,prop))
+                        FreeCAD.ActiveDocument.recompute()
+                        newobj.Placement = cloned.Placement.multiply(placement)
+                # update/reset view properties too? no i think...
+                #for prop in cloned.ViewObject.PropertiesList:
+                #    if not prop in ["Proxy"]:
+                #        setattr(newobj.ViewObject,prop,getattr(cloned.ViewObject,prop))
+
+                # update objects relating to this one
+                for parent in obj.InList:
+                    for prop in parent.PropertiesList:
+                        if getattr(parent,prop) == obj:
+                            setattr(parent,prop,newobj)
+                            FreeCAD.Console.PrintMessage("Object "+parent.Label+"'s reference to this object has been updated\n")
+                        elif isinstance(getattr(parent,prop),list) and (obj in getattr(parent,prop)):
+                            if (prop == "Group") and hasattr(parent,"addObject"):
+                                parent.addObject(newobj)
+                            else:
+                                g = getattr(parent,prop)
+                                g.append(newobj)
+                                setattr(parent,prop,g)
+                            FreeCAD.Console.PrintMessage("Object "+parent.Label+"'s reference to this object has been updated\n")
+                        # TODO treat PropertyLinkSub / PropertyLinkSubList DANGEROUS - toponaming
+
+                # remove old object if needed, and relabel new object
+                if newobj != obj:
+                    name = obj.Name
+                    label = obj.Label
+                    from DraftGui import todo
+                    FreeCAD.ActiveDocument.removeObject(name)
+                    newobj.Label = label
+
+                # commit changes
+                FreeCAD.ActiveDocument.commitTransaction()
+                FreeCAD.ActiveDocument.recompute()
+
+            elif Draft.getType(obj) == "Clone":
+                FreeCAD.Console.PrintError(translate("BIM","Draft Clones are not supported yet!")+"\n")
+            else:
+                FreeCAD.Console.PrintError(translate("BIM","The selected object is not a clone")+"\n")
+        else:
+            FreeCAD.Console.PrintError(translate("BIM","Please select exactly one object")+"\n")

BimCommands.py
@@ -90,7 +90,7 @@ class BIM_Sketch:
 
     def GetResources(self):
 
-        return {'Pixmap'  : ":/icons/Sketcher_NewSketch.svg",
+        return {'Pixmap'  : os.path.join(os.path.dirname(__file__),"icons","Sketch.svg"),
                 'MenuText': QT_TRANSLATE_NOOP("BIM_Sketch", "Sketch"),
                 'ToolTip' : QT_TRANSLATE_NOOP("BIM_Sketch", "Creates a new sketch in the current working plane"),
                 'Accel'   : 'S,K'}

BimIfcProperties.py
@@ -24,13 +24,9 @@
 
 """This module contains FreeCAD commands for the BIM workbench"""
 
-import sys,os,FreeCAD,FreeCADGui,Arch_rc,csv
-from PySide import QtCore,QtGui
-from DraftTools import translate
-
-def QT_TRANSLATE_NOOP(ctx,txt): return txt # dummy function for the QT translator
-
-
+import os
+import FreeCAD
+from BimTranslateUtils import *
 
 class BIM_IfcProperties:
 
@@ -53,6 +49,8 @@ class BIM_IfcProperties:
 
     def Activated(self):
 
+        import FreeCADGui
+        from PySide import QtCore,QtGui
         try:
             import ArchIFC
             self.ifcroles = ArchIFC.IfcTypes
@@ -126,6 +124,7 @@ class BIM_IfcProperties:
 
     def rebuildObjectsList(self):
 
+        import FreeCADGui
         # build objects list and fill search terms
         objectslist = {}
         searchterms = [""]
@@ -157,8 +156,6 @@ class BIM_IfcProperties:
 
         "updates the tree widgets in all tabs"
 
-        import Draft
-
         self.model.clear()
         self.model.setHorizontalHeaderLabels([translate("BIM","Label"),translate("BIM","IFC type"),translate("BIM","Search results")])
         #self.form.tree.header().setResizeMode(QtGui.QHeaderView.Stretch)
@@ -178,6 +175,7 @@ class BIM_IfcProperties:
     def readFromCSV(self,csvfile):
         """reads a csv file and returns a dict"""
 
+        import csv
         result = {}
         if os.path.exists(csvfile):
             with open(csvfile, "r") as f:
@@ -188,6 +186,7 @@ class BIM_IfcProperties:
 
     def updateByType(self):
 
+        from PySide import QtCore,QtGui
         groups = {}
         for name,role in self.objectslist.items():
             role = role[0]
@@ -226,6 +225,7 @@ class BIM_IfcProperties:
 
     def updateByTree(self):
 
+        from PySide import QtCore,QtGui
         # order by hierarchy
         def istop(obj):
             for parent in obj.InList:
@@ -289,6 +289,7 @@ class BIM_IfcProperties:
 
     def updateDefault(self):
 
+        from PySide import QtCore,QtGui
         for name,role in self.objectslist.items():
             role = role[0]
             obj = FreeCAD.ActiveDocument.getObject(name)
@@ -340,6 +341,7 @@ class BIM_IfcProperties:
 
     def getSearchResults(self,obj):
 
+        from PySide import QtCore,QtGui
         text = self.form.searchField.currentText()
         if not text:
             return QtGui.QStandardItem()
@@ -372,8 +374,8 @@ class BIM_IfcProperties:
 
     def updateProperties(self,sel1=None,sel2=None):
 
+        from PySide import QtCore,QtGui
         self.propmodel.clear()
-
         self.propmodel.setHorizontalHeaderLabels([QtGui.QApplication.translate("Arch", "Property", None),
                                                  QtGui.QApplication.translate("Arch", "Type", None),
                                                  QtGui.QApplication.translate("Arch", "Value", None)])
@@ -517,6 +519,7 @@ class BIM_IfcProperties:
 
     def addProperty(self,idx=0,pset=None,prop=None,ptype=None):
 
+        from PySide import QtCore,QtGui
         if not self.form.tree.selectedIndexes():
             return
         if not pset:
@@ -565,6 +568,7 @@ class BIM_IfcProperties:
 
     def addPset(self,idx):
 
+        from PySide import QtCore,QtGui
         if not self.form.tree.selectedIndexes():
             return
         if idx == 1:
@@ -598,6 +602,7 @@ class BIM_IfcProperties:
 
     def removeProperty(self):
 
+        from PySide import QtCore,QtGui
         sel = self.form.treeProperties.selectedIndexes()
         remove = []
         if sel:
@@ -628,109 +633,113 @@ class BIM_IfcProperties:
         FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/BIM").SetInt("IfcPropertiesVisibleState",index)
         self.update()
 
+if FreeCAD.GuiUp:
 
-class propertiesDelegate(QtGui.QStyledItemDelegate):
+    from PySide import QtCore,QtGui
 
-
-    def __init__(self, parent=None, container=None, ptypes=[], plabels=[], *args):
-
-        self.container = container
-        QtGui.QStyledItemDelegate.__init__(self, parent, *args)
-        self.ptypes = ptypes
-        self.plabels = plabels
-
-    def createEditor(self,parent,option,index):
-
-        if index.column() == 0: # property name
-            editor = QtGui.QLineEdit(parent)
-        elif index.column() == 1: # property type
-            editor = QtGui.QComboBox(parent)
-        else: # property value
-            ptype = index.sibling(index.row(),1).data()
-            if "Integer" in ptype:
-                editor = QtGui.QSpinBox(parent)
-            elif "Real" in ptype:
-                editor = QtGui.QDoubleSpinBox(parent)
-                editor.setDecimals(FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units").GetInt("Decimals",2))
-            elif ("Boolean" in ptype) or ("Logical" in ptype):
-                editor = QtGui.QComboBox(parent)
-                editor.addItems(["True","False"])
-            elif "Measure" in ptype:
-                editor = FreeCADGui.UiLoader().createWidget("Gui::InputField")
-                editor.setParent(parent)
-            else:
+    class propertiesDelegate(QtGui.QStyledItemDelegate):
+    
+    
+        def __init__(self, parent=None, container=None, ptypes=[], plabels=[], *args):
+    
+            self.container = container
+            QtGui.QStyledItemDelegate.__init__(self, parent, *args)
+            self.ptypes = ptypes
+            self.plabels = plabels
+    
+        def createEditor(self,parent,option,index):
+    
+            if index.column() == 0: # property name
                 editor = QtGui.QLineEdit(parent)
-            editor.setObjectName("editor_"+ptype)
-        return editor
-
-    def setEditorData(self, editor, index):
-
-        if index.column() == 0:
-            editor.setText(index.data())
-        elif index.column() == 1:
-            editor.addItems(self.plabels)
-            if index.data() in self.plabels:
-                idx = self.plabels.index(index.data())
-                editor.setCurrentIndex(idx)
-        else:
-            if "Integer" in editor.objectName():
-                try:
-                    editor.setValue(int(index.data()))
-                except (ValueError,AttributeError):
-                    editor.setValue(0)
-            elif "Real" in editor.objectName():
-                try:
-                    editor.setValue(float(index.data()))
-                except (ValueError,AttributeError):
-                    editor.setValue(0)
-            elif ("Boolean" in editor.objectName()) or ("Logical" in editor.objectName()):
-                try:
-                    editor.setCurrentIndex(["true","false"].index(index.data().lower()))
-                except (ValueError,AttributeError):
-                    editor.setCurrentIndex(1)
-            elif "Measure" in editor.objectName():
-                try:
-                    editor.setText(index.data())
-                except (ValueError,AttributeError):
-                    editor.setValue(0)
-            else:
+            elif index.column() == 1: # property type
+                editor = QtGui.QComboBox(parent)
+            else: # property value
+                ptype = index.sibling(index.row(),1).data()
+                if "Integer" in ptype:
+                    editor = QtGui.QSpinBox(parent)
+                elif "Real" in ptype:
+                    editor = QtGui.QDoubleSpinBox(parent)
+                    editor.setDecimals(FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Units").GetInt("Decimals",2))
+                elif ("Boolean" in ptype) or ("Logical" in ptype):
+                    editor = QtGui.QComboBox(parent)
+                    editor.addItems(["True","False"])
+                elif "Measure" in ptype:
+                    editor = FreeCADGui.UiLoader().createWidget("Gui::InputField")
+                    editor.setParent(parent)
+                else:
+                    editor = QtGui.QLineEdit(parent)
+                editor.setObjectName("editor_"+ptype)
+            return editor
+    
+        def setEditorData(self, editor, index):
+    
+            if index.column() == 0:
                 editor.setText(index.data())
-
-    def setModelData(self, editor, model, index):
-
-        remove = []
-        if index.column() == 0:
-            oldtext = index.data()
-            if oldtext != editor.text():
-                remove.append(oldtext)
-            basename = editor.text()
-            # check for duplicate name
-            names = []
-            for i in range(model.rowCount()):
-                topitem = model.item(i,0)
-                names.append(topitem.text())
-                if topitem.hasChildren():
-                    for j in range(topitem.rowCount()):
-                        childitem = topitem.child(j,0)
-                        names.append(childitem.text())
-            suffix = 1
-            newname = basename
-            while newname in names:
-                newname = basename + str(suffix).zfill(3)
-                suffix += 1
-            model.setData(index,newname)
-        elif index.column() == 1:
-            if editor.currentIndex() > -1:
-                idx = editor.currentIndex()
-                data = self.plabels[idx]
-                model.setData(index,data)
-        else:
-            if ("Integer" in editor.objectName()) or ("Real" in editor.objectName()):
-                model.setData(index,str(editor.value()))
-            elif ("Boolean" in editor.objectName()) or ("Logical" in editor.objectName()):
-                model.setData(index,editor.currentText())
-            elif "Measure" in editor.objectName():
-                model.setData(index,editor.property("text"))
+            elif index.column() == 1:
+                editor.addItems(self.plabels)
+                if index.data() in self.plabels:
+                    idx = self.plabels.index(index.data())
+                    editor.setCurrentIndex(idx)
             else:
-                model.setData(index,editor.text())
-        self.container.updateDicts(remove)
+                if "Integer" in editor.objectName():
+                    try:
+                        editor.setValue(int(index.data()))
+                    except (ValueError,AttributeError):
+                        editor.setValue(0)
+                elif "Real" in editor.objectName():
+                    try:
+                        editor.setValue(float(index.data()))
+                    except (ValueError,AttributeError):
+                        editor.setValue(0)
+                elif ("Boolean" in editor.objectName()) or ("Logical" in editor.objectName()):
+                    try:
+                        editor.setCurrentIndex(["true","false"].index(index.data().lower()))
+                    except (ValueError,AttributeError):
+                        editor.setCurrentIndex(1)
+                elif "Measure" in editor.objectName():
+                    try:
+                        editor.setText(index.data())
+                    except (ValueError,AttributeError):
+                        editor.setValue(0)
+                else:
+                    editor.setText(index.data())
+    
+        def setModelData(self, editor, model, index):
+    
+            remove = []
+            if index.column() == 0:
+                oldtext = index.data()
+                if oldtext != editor.text():
+                    remove.append(oldtext)
+                basename = editor.text()
+                # check for duplicate name
+                names = []
+                for i in range(model.rowCount()):
+                    topitem = model.item(i,0)
+                    names.append(topitem.text())
+                    if topitem.hasChildren():
+                        for j in range(topitem.rowCount()):
+                            childitem = topitem.child(j,0)
+                            names.append(childitem.text())
+                suffix = 1
+                newname = basename
+                while newname in names:
+                    newname = basename + str(suffix).zfill(3)
+                    suffix += 1
+                model.setData(index,newname)
+            elif index.column() == 1:
+                if editor.currentIndex() > -1:
+                    idx = editor.currentIndex()
+                    data = self.plabels[idx]
+                    model.setData(index,data)
+            else:
+                if ("Integer" in editor.objectName()) or ("Real" in editor.objectName()):
+                    model.setData(index,str(editor.value()))
+                elif ("Boolean" in editor.objectName()) or ("Logical" in editor.objectName()):
+                    model.setData(index,editor.currentText())
+                elif "Measure" in editor.objectName():
+                    model.setData(index,editor.property("text"))
+                else:
+                    model.setData(index,editor.text())
+            self.container.updateDicts(remove)
+    

BimIfcQuantities.py
@@ -24,11 +24,9 @@
 
 """This module contains FreeCAD commands for the BIM workbench"""
 
-import os,FreeCAD,FreeCADGui,Arch_rc
-from PySide import QtCore,QtGui
-from DraftTools import translate
-
-def QT_TRANSLATE_NOOP(ctx,txt): return txt # dummy function for the QT translator
+import os
+import FreeCAD
+from BimTranslateUtils import *
 
 qprops = ["Length","Width","Height","Area","HorizontalArea","VerticalArea","Volume"] # quantities columns
 trqprops = [translate("BIM","Length"),
@@ -61,6 +59,8 @@ class BIM_IfcQuantities:
 
     def Activated(self):
 
+        import FreeCADGui
+        from PySide import QtCore,QtGui
         # build objects list
         self.objectslist = {}
         for obj in FreeCAD.ActiveDocument.Objects:
@@ -105,6 +105,8 @@ class BIM_IfcQuantities:
         
         "updates the tree widgets in all tabs"
 
+        import FreeCADGui
+        from PySide import QtCore,QtGui
         import Draft
 
         # quantities tab - only fill once
@@ -232,6 +234,7 @@ class BIM_IfcQuantities:
 
     def onClickTree(self,index=None):
 
+        import FreeCADGui
         FreeCADGui.Selection.clearSelection()
         sel = self.form.quantities.selectedIndexes()
         for index in sel:

commit 249e31886620d6b38f87caaae673577df10dea1e

IFC Explorer: Fix regression introduced in 8bb8bb5a

BimIfcExplorer.py
@@ -223,6 +223,7 @@ class BIM_IfcExplorer:
         "inserts selected objects in the active document"
         
         import importIFC
+        from PySide import QtCore,QtGui
         doc = FreeCAD.ActiveDocument
         if doc and self.filename:
             item = self.tree.currentItem()
@@ -570,6 +571,8 @@ class BIM_IfcExplorer:
     def onDoubleClickTree(self,item,column):
 
         "when a property or attribute is double-clicked"
+
+        from PySide import QtCore,QtGui
 
         if self.tree:
             txt = item.text(column)